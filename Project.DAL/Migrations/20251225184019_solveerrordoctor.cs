599c1e39a79a8ec0ef60fdd02ecd3f7eb37a14fa
MohamedAbdalRaouf <mohamedaita962@gmail.com>
2025-12-25 22:27:18 +0200

    Add doctor

---
+++ Project.DAL/Migrations/20251225184019_solveerrordoctor.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Project.DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class solveerrordoctor : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<TimeSpan>(
+                name: "WorkEnd",
+                table: "Doctors",
+                type: "time",
+                nullable: false,
+                defaultValue: new TimeSpan(0, 0, 0, 0, 0));
+
+            migrationBuilder.AddColumn<TimeSpan>(
+                name: "WorkStart",
+                table: "Doctors",
+                type: "time",
+                nullable: false,
+                defaultValue: new TimeSpan(0, 0, 0, 0, 0));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "WorkEnd",
+                table: "Doctors");
+
+            migrationBuilder.DropColumn(
+                name: "WorkStart",
+                table: "Doctors");
+        }
+    }
+}
